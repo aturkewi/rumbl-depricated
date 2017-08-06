408276c27f3b4f54ef6f22e4b35c106ad8e4ff2c
Avidor Turkewitz <aviturkewitz@gmail.com>
2017-08-06 15:05:58 -0400

    Add login on create

--- web/controllers/user_controller.ex
+++ web/controllers/user_controller.ex
@@ -1,15 +1,12 @@
 defmodule Rumbl.UserController do
   use Rumbl.Web, :controller
+  plug :authenticate when action in [:index, :show]
   alias Rumbl.User
   
+  
   def index(conn, _params) do
-    case authenticate(conn) do
-      %Plug.Conn{halted: true} = conn ->
-        conn
-      conn ->
-        users = Repo.all(Rumbl.User)
-        render conn, "index.html", users: users
-    end
+    users = Repo.all(Rumbl.User)
+    render conn, "index.html", users: users
   end
   
   def show(conn, %{"id" => id}) do
@@ -27,6 +24,7 @@ defmodule Rumbl.UserController do
     case Repo.insert(changeset) do
       {:ok, user} ->
         conn
+        |> Rumbl.Auth.login(user)
         |> put_flash(:info, "#{user.name} create!")
         |> redirect(to: user_path(conn, :index))
       {:error, changeset} ->
@@ -34,10 +32,14 @@ defmodule Rumbl.UserController do
     end
   end
   
-  defp authenticate(conn) do
-    conn
-    |> put_flash(:error, "You much be logged in to access that page")
-    |> redirect(to: page_path(conn, :index))
-    |>halt()
+  defp authenticate(conn, _opts) do
+    if conn.assigns.current_user do
+      conn
+    else
+      conn
+      |> put_flash(:error, "You much be logged in to access that page")
+      |> redirect(to: page_path(conn, :index))
+      |>halt()
+    end
   end
 end
