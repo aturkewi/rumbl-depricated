fe1e00d99c0d8d73b3e9111fe52cf657356ea52a
Avidor Turkewitz <aviturkewitz@gmail.com>
2017-08-27 17:28:50 -0400

    Add video creation controller tests

--- test/controllers/video_controller_test.exs
+++ test/controllers/video_controller_test.exs
@@ -1,6 +1,23 @@
 defmodule Rumbl.VideoControllerTest do
   use Rumbl.ConnCase
   
+  alias Rumbl.Video
+  
+  @valid_attrs %{url: "http://youtu.be", title: "vid", description: "a vid"}
+  @invalid_attrs %{title: "invalid"}
+  
+  defp video_count(query), do: Repo.one(from v in query, select: count(v.id))
+  
+  setup %{conn: conn} = config do
+    if username = config[:login_as] do
+      user = insert_user(username: username)
+      conn = assign(conn(), :current_user, user)
+      {:ok, conn: conn, user: user}
+    else
+      :ok
+    end
+  end
+  
   test "requires user authentication on all actions", %{conn: conn} do
     Enum.each([
       get(conn, video_path(conn, :new)),
@@ -16,16 +33,6 @@ defmodule Rumbl.VideoControllerTest do
     end)
   end
   
-  setup %{conn: conn} = config do
-    if username = config[:login_as] do
-      user = insert_user(username: username)
-      conn = assign(conn(), :current_user, user)
-      {:ok, conn: conn, user: user}
-    else
-      :ok
-    end
-  end
-  
   @tag login_as: "max"
   test "lists all user's videos on index", %{conn: conn, user: user} do
     user_video = insert_video(user, title: "funny cats")
@@ -35,5 +42,20 @@ defmodule Rumbl.VideoControllerTest do
     assert html_response(conn, 200) =~ ~r/Listing videos/
     assert String.contains?(conn.resp_body, user_video.title)
     refute String.contains?(conn.resp_body, other_video.title)
+  end
+  
+  @tag login_as: "max"
+  test "creates user video and redirects", %{conn: conn, user: user} do
+    conn = post conn, video_path(conn, :create), video: @valid_attrs
+    assert redirected_to(conn) == video_path(conn, :index)
+    assert Repo.get_by!(Video, @valid_attrs).user_id == user.id
+  end
+  
+  @tag login_as: "max"
+  test "does not create video and renders errors when invalid", %{conn: conn} do
+    count_before = video_count(Video)
+    conn = post conn, video_path(conn, :create), video: @invalid_attrs
+    assert html_response(conn, 200) =~ "check the errors"
+    assert video_count(Video) == count_before
   end
 end
