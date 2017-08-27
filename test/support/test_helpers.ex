e192cdfc554bcd7bb0cddd8ec7cd77d84d2605eb
Avidor Turkewitz <aviturkewitz@gmail.com>
2017-08-27 17:09:21 -0400

    Fix crypto method call

--- test/support/test_helpers.ex
+++ test/support/test_helpers.ex
@@ -4,7 +4,7 @@ defmodule Rumbl.TestHelpers do
   def insert_user(attrs \\ %{}) do
     changes = Dict.merge(%{
       name: "Some user",
-      username: "user#{Base.encode16(:crypto.rand_bytes(8 ))}",
+      username: "user#{Base.encode16(:crypto.strong_rand_bytes(8))}",
       password: "password"
     }, attrs)
     %Rumbl.User{}
@@ -12,7 +12,7 @@ defmodule Rumbl.TestHelpers do
     |> Repo.insert!()
   end
   
-  def inser_video(user, attrs \\ %{}) do
+  def insert_video(user, attrs \\ %{}) do
     user
     |> Ecto.build_assoc(:videos, attrs)
     |> Repo.insert!()
