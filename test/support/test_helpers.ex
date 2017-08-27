13506b173a52ce82dd8d3eb9cb74cb7bc6c6993c
Avidor Turkewitz <aviturkewitz@gmail.com>
2017-08-27 16:41:07 -0400

    Get rid of unused tests, add helper

---
+++ test/support/test_helpers.ex
@@ -0,0 +1,20 @@
+defmodule Rumbl.TestHelpers do
+  alias  Rumbl.Repo
+  
+  def insert_user(attrs \\ %{}) do
+    changes = Dict.merge(%{
+      name: "Some user",
+      username: "user#{Base.encode16(:crypto.rand_bytes(8 ))}",
+      password: "password"
+    }, attrs)
+    %Rumbl.User{}
+    |> Rumbl.User.registration_changeset(changes)
+    |> Repo.insert!()
+  end
+  
+  def inser_video(attrs \\ %{}) do
+    user
+    |> Ecto.build_assoc(:videos, attrs)
+    |> Repo.insert!()
+  end
+end
