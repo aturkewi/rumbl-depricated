53cf814bea13c2fe361122c3728f678e22b60ed3
Avidor Turkewitz <aviturkewitz@gmail.com>
2017-09-10 11:09:00 -0400

    Joining channel for video - no auth

--- web/static/js/player.js
+++ web/static/js/player.js
@@ -1,6 +1,6 @@
 let Player = {
   player: null,
-  
+
   init(domId, playerId, onReady){
     window.onYouTubeIframeAPIReady = () => {
       this.onIframeReady(domId, playerId, onReady)
@@ -9,19 +9,19 @@ let Player = {
     youtubeScriptTag.src = "//www.youtube.com/iframe_api"
     document.head.appendChild(youtubeScriptTag)
   },
-  
+
   onIframeReady(domId, playerId, onReady){
     this.player = new YT.Player(domId, {
       height: "360",
       width: "420",
       videoId: playerId,
       events: {
-        "onRedy": (event => onReady(events)),
+        "onReady": (event => onReady(event)),
         "onStateChange": (event => this.onPlayerStateChange(event))
       }
     })
   },
-  
+
   onPlayerStateChange(event){ },
   getCurrentTime(){ return Math.floor(this.player.getCurrentTime() * 1000)},
   seekTo(millsec){ return this.player.seekTo(millsec / 1000)}
