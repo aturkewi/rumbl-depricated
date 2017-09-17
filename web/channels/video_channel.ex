435b957507ad42f80163c9411f7b80c79d7d9e27
Avidor Turkewitz <aviturkewitz@gmail.com>
2017-09-17 15:56:41 -0400

    Caught up - copy paste from source

--- web/channels/video_channel.ex
+++ web/channels/video_channel.ex
@@ -1,26 +1,53 @@
+#---
+# Excerpted from "Programming Phoenix",
+# published by The Pragmatic Bookshelf.
+# Copyrights apply to this code. It may not be used to create training material,
+# courses, books, articles, and the like. Contact us if you are in doubt.
+# We make no guarantees that this code is fit for any purpose.
+# Visit http://www.pragmaticprogrammer.com/titles/phoenix for more book information.
+#---
 defmodule Rumbl.VideoChannel do
   use Rumbl.Web, :channel
+  alias Rumbl.AnnotationView
 
-  def join("videos:" <> video_id, _params, socket) do
-    # :timer.send_interval(5_000, :ping)
-    {:ok, socket}
-    # {:ok, assign(socket, :video_id, String.to_integer(video_id))}
+  def join("videos:" <> video_id, params, socket) do
+    last_seen_id = params["last_seen_id"] || 0
+    video_id = String.to_integer(video_id)
+    video = Repo.get!(Rumbl.Video, video_id)
+
+    annotations = Repo.all(
+           from a in assoc(video, :annotations),
+         where: a.id > ^last_seen_id,
+      order_by: [asc: a.at, asc: a.id],
+         limit: 200,
+       preload: [:user]
+    )
+
+    resp = %{annotations: Phoenix.View.render_many(annotations, AnnotationView,
+                                                   "annotation.json")}
+    {:ok, resp, assign(socket, :video_id, video_id)}
   end
 
   def handle_in("new_annotation", params, socket) do
-    broadcast! socket, "new_annotation", %{
-      user: %{username: "annon"},
-      body: params["body"],
-      at: params["at"]
-    }
+    user = Rumbl.Repo.get(Rumbl.User, socket.assigns.user_id)
 
-    {:reply, :ok, socket}
+    changeset =
+      user
+      |> build_assoc(:annotations, video_id: socket.assigns.video_id)
+      |> Rumbl.Annotation.changeset(params)
+
+    case Repo.insert(changeset) do
+      {:ok, annotation} ->
+        broadcast! socket, "new_annotation", %{
+          id: annotation.id,
+          user: Rumbl.UserView.render("user.json", %{user: user}),
+          body: annotation.body,
+          at: annotation.at
+        }
+        {:reply, :ok, socket}
+
+      {:error, changeset} ->
+        {:reply, {:error, %{errors: changeset}}, socket}
+    end
   end
-
-  # def handle_info(:ping, socket) do
-  #   count = socket.assigns[:count] || 1
-  #   push socket, "ping", %{count: count}
-  #
-  #   {:noreply, assign(socket, :count, count + 1)}
-  # end
 end
--- web/models/user.ex
+++ web/models/user.ex
@@ -1,24 +1,25 @@
 defmodule Rumbl.User do
   use Rumbl.Web, :model
   # defstruct [:id, :name, :username, :password]
-  
+
   schema "users" do
     field :name, :string
     field :username, :string
     field :password, :string, virtual: true
     field :password_hash, :string
     has_many :videos, Rumbl.Video
-    
+    has_many :annotations, Rumbl.Annotation
+
     timestamps()
   end
-  
+
   def changeset(model, params \\ :empty) do
     model
     |> cast(params, ~w(name username), [])
     |> validate_length(:username, min: 1, max: 20)
     |> unique_constraint(:username)
   end
-  
+
   def registration_changeset(model, params) do
     model
     |> changeset(params)
@@ -26,7 +27,7 @@ defmodule Rumbl.User do
     |> validate_length(:password, min: 6, max: 100)
     |> put_pass_hash()
   end
-  
+
   defp put_pass_hash(changeset) do
     case changeset do
       %Ecto.Changeset{valid?: true, changes: %{password: pass}} ->
